b32be3fe630aedf0adef86cb281ab2c171d0549a
Santiago <santiagomejiad19@hotmail.com>
2020-11-13 17:22:02 -0500

    Creating new Badges with POST

--- src/components/PageError.js
+++ src/components/PageError.js
@@ -1,9 +1,10 @@
+  
 import React from 'react';
 
 import './styles/PageError.css';
 
 function PageError(props) {
-  return <div className="PageError">❌{props.error.message}😱</div>;
+  return <div className="PageError">{props.error.message}</div>;
 }
 
 export default PageError;
